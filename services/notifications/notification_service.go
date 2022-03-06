8727145524049dcd0e79aef359ec0073fd96afb5
EdwardLopezLua <edward_lopez_cntcts@hotmail.com>
2022-03-05 19:01:57 -0500

    v1.5

--- services/notifications/notification_service.go
+++ services/notifications/notification_service.go
@@ -17,10 +17,10 @@ func AddNotification_Service(notification models.Mo_Notifications) (int, bool, s
 	//Obtenemos las categorias
 	error_update := notification_repository.Mo_Add(notification)
 	if error_update != nil {
-		return 500, true, "Error en el servidor interno al intentar agregar el tip, detalles: " + error_update.Error(), ""
+		return 500, true, "Error en el servidor interno al intentar agregar el Notificacion, detalles: " + error_update.Error(), ""
 	}
 
-	return 201, false, "", "Tip agregado correctamente"
+	return 201, false, "", "Notificacion agregado correctamente"
 }
 
 func ShowNotification_Service(iduser_int int, page_int int, idtype_int int) (int, bool, string, []*models.Mo_NotificationShow) {
@@ -34,7 +34,7 @@ func ShowNotification_Service(iduser_int int, page_int int, idtype_int int) (int
 
 	error_update := notification_repository.Mo_Update(iduser_int, idtype_int)
 	if error_update != nil {
-		return 500, true, "Error en el servidor interno al intentar actualizar el estado de las notificaciones, detalles: " + error_update.Error(), notification_var
+		return 500, true, "Error en el servidor interno al intentar actualizar el estado de las notificacion, detalles: " + error_update.Error(), notification_var
 	}
 
 	return 201, false, "", notifications
